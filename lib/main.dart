7c262ca69f260005a3345cd89ddfc9ebc127ee35
Tasmia_Nasir <tasmianehelin@gmail.com>
2026-07-06 21:09:41 +0600

    Splash screen
    Splash screen design and Slight modification done in registration page ( made hovering input field, added animation)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/registration_page.dart';
+import 'widgets/app_lifecycle_splash_wrapper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,9 @@ class MyApp extends StatelessWidget {
           child: Container(
             width: 430,
             constraints: const BoxConstraints(maxWidth: 430),
-            child: const RegistrationPage(),
+            child: const AppLifecycleSplashWrapper(
+              child: RegistrationPage(),
+            ),
           ),
         ),
       ),
