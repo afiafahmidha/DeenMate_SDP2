cbb8aae8a22980bf23b969a475b08b4e51242c27
Tasmia_Nasir <tasmianehelin@gmail.com>
2026-08-02 23:23:21 +0600

    Add AppColors palette

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color navyBlue = Color(0xFF0A2647);
+  static const Color midTeal = Color(0xFF2C7A7B);
+  static const Color dustyBlueTeal = Color(0xFF497D8A);
+  static const Color coralOrange = Color(0xFFE07A5F);
+  static const Color goldenAmber = Color(0xFFD4A373);
+  static const Color softCream = Color(0xFFF4F1DE);
+  static const Color sageGreen = Color(0xFF81B29A);
+  static const Color placeholder = Color(0xFFB0BEC5);
+  static const Color white = Colors.white;
+}
